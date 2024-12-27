8ca4f915b9d9bd07cfc9e98035df4972c5ddee35
Md. Rahim Uddin <rahimuddinduet90@gmail.com>
2024-12-27 23:29:46 +0600

    Final update the user model

--- User/View/user_others_api.py
+++ User/View/user_others_api.py
@@ -1,52 +1,31 @@
-from rest_framework.viewsets import ViewSet
-
-from User.models import Users 
-
-from rest_framework.permissions import IsAuthenticated 
-from User.Serializer.user_support_serializer import UserSerializer 
-from User.models import Users 
-from rest_framework.response import Response 
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.pagination import PageNumberPagination 
+from rest_framework.permissions import IsAuthenticated
+
+from User.models import Users
+from User.Serializer.user_support_serializer import UserSerializer
 
 
 
-class UserAPIViewSet(ViewSet):
+class UserAPIViewSet(ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+   
 
-
-    # here using list api url 
-    def list(self,request):
-        paginator = PageNumberPagination()
-        # define paginator size 
-        paginator.page_size = 10
-
-        # fetch query set 
-        users  = Users.objects.all()
-
-        result_page = paginator.paginate_queryset(users, request)
-
-        # serialize the paginated queryset 
-        serializer  =UserSerializer(result_page,many=True)
-
-        return paginator.get_paginated_response(serializer.data)
-    
-
-    # api request for update request 
-
-    def patch(self,request,pk=None):
+    def partial_update(self, request, *args, **kwargs):
+        """Handle PATCH requests to update a user."""
         try:
-            user = Users.objects.get(pk=pk)
-
+            user = self.get_object()  # Retrieve the user by primary key
         except Users.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-        
 
-        serializer = UserSerializer(user,data=request.data )
+        serializer = self.get_serializer(user, data=request.data, partial=True)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
