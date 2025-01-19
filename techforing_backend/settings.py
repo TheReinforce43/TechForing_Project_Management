e4137f6a556b50f32189bce3881e09f16b1cbba9
Md. Rahim Uddin <rahimuddinduet90@gmail.com>
2025-01-19 21:41:25 +0600

    updated the file

--- techforing_backend/settings.py
+++ techforing_backend/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://techforing-project-management.onrender.com/','http://127.0.0.1:8000/']
 
 
 # Application definition
